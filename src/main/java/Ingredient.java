b3813b0d00049ac02d1b98fab7f2b10768586f58
Angela-Haim <hei.angela.5@gmail.com>
2026-01-28 14:10:37 +0300

    chore: modification of DataRetriever and Ingredient

--- src/main/java/Ingredient.java
+++ src/main/java/Ingredient.java
@@ -6,21 +6,65 @@ public class Ingredient {
     private Integer id;
     private String name;
     private CategoryEnum category;
-    private double price;
-    private Dish dish;
-    private Double requiredQuantity; // quantité pour le plat
+    private Double price;
 
-
-
-    public Ingredient(Integer id, String name, CategoryEnum category, double price, Dish dish, Double requiredQuantity) {
-
-        this.id = id;
-        this.name = name;
-        this.category = category;
-        this.price = price;
-        this.dish = dish;
-        this.requiredQuantity = requiredQuantity;
+    public Ingredient() {
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && category == that.category && Objects.equals(price, that.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", price=" + price +
+                '}';
+    }
+}
 
 
     public Integer getId() {
