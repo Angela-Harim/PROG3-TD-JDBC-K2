757a6371b64b685e64f49c4b7ac79af8b3934213
Angela-Haim <hei.angela.5@gmail.com>
2026-01-29 17:06:05 +0300

    feat: add payment status and sale creation from paid order

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -8,10 +8,10 @@ public class DBConnection {
 
     public Connection getConnection() {
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+            String jdbcURL = System.getenv("JDBC_URL"); //
+            String user = System.getenv("jdbc:postgresql://localhost:5432/mini_dish_db"); //mini_dish_db_manager
+            String password = System.getenv("123456"); //123456
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "angela");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
